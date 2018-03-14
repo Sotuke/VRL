90c24060662515ccd85da6b62945e2b75b3a0486
Paul <oliibert@hotmail.com>
2018-03-14 13:01:28 +0200

    proov

---
+++ src/main/java/com/blopto/web/blopto/BloptoApplication.java
@@ -0,0 +1,12 @@
+package com.blopto.web.blopto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloptoApplication.class, args);
+	}
+}
