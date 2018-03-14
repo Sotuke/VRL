147e8217650863df7e7b4bdc4b1dd026b485a8e2
Andri <andri@tfwno.gf>
2018-03-14 16:32:28 +0200

    hkjlhgj

--- src/main/java/com/blopto/web/blopto/BloptoApplication.java
+++ src/main/java/com/blopto/web/blopto/BloptoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BloptoApplication {
 
 	public static void main(String[] args) {
-		//test
+		//test kjlgh
 		SpringApplication.run(BloptoApplication.class, args);
 	}
 }
