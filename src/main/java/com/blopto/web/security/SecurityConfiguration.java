f7309170feba39255973cb6abe4d3fd62ab76ddc
Kermo Laurits <kermo.laurits@guardtime.com>
2018-03-26 21:49:33 +0300

    testing fb vol7

--- src/main/java/com/blopto/web/security/SecurityConfiguration.java
+++ src/main/java/com/blopto/web/security/SecurityConfiguration.java
@@ -10,15 +10,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
 import org.springframework.social.connect.web.ProviderSignInController;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -33,15 +31,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private FacebookConnectionSignup facebookConnectionSignup;
 
-/*
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // whitelist everything for now
-        http.authorizeRequests().antMatchers("/**").permitAll();
-        http.cors().and().csrf().disable();
-
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
-    */
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -56,16 +49,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout();
     } // @formatter:on
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     // @Primary
