49ff1c5c4a70207647c471383df39dc4c76bcff2
Paul <oliibert@hotmail.com>
2018-04-17 22:50:35 +0300

    Kas see töötab?

--- src/main/java/com/blopto/web/controller/AboutController.java
+++ src/main/java/com/blopto/web/controller/AboutController.java
@@ -1,13 +1,32 @@
 package com.blopto.web.controller;
 
+import com.blopto.web.bean.Statistics;
+import com.blopto.web.repository.StatisticsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AboutController {
 
+    private final StatisticsRepository repository;
+
+    @Autowired
+    AboutController(StatisticsRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/about")
-    public String getMap() {
+    public String getMap(Model model) {
+        int queryLimit = 1;
+        Statistics e = repository.findMostUsedBrowser(new PageRequest(0, queryLimit));
+        if (e != null) {
+            model.addAttribute("browser", e);
+        }
         return "about";
     }
 }
