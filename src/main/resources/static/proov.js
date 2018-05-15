4f9e376132f113267428876b6b3ab68d1d277ca0
Paul <oliibert@hotmail.com>
2018-05-16 00:14:17 +0300

    kas sa nüüd töötad?

--- src/main/resources/static/proov.js
+++ src/main/resources/static/proov.js
@@ -1,14 +1,3 @@
-function checkUpdates(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/api/test");
-    xhr.onload = function() {
-        var serverResponse = JSON.parse(xhr.responseText);
-        document.getElementById("p1").innerHTML = serverResponse.number;
-    };
-    xhr.send();
-}
-
-setInterval(function() {checkUpdates('/updates')}, 10000);
 
 console.log("I am in the file");
 
